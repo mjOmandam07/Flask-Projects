1a21ba3eeb1ce07cdf960defddb8ca9e799c6659
mjOmandam07 <mjomandam@gmail.com>
2020-11-05 22:55:16 +0800

    fixed the update route, added the choices for the selectFields and added the conditions if the college = 'SGS' and connected it to the update query

--- student database/studentapp/routes.py
+++ student database/studentapp/routes.py
@@ -32,100 +32,174 @@ def register():
     db = models.students()
     college = db.showCollege()
     form = registerForm()
-    form.register_college.choices = [(item[0]) for item in college]
+    college_arr = [('','Choose...'),]
+    for item in college:
+        college_arr.append((item[0],("("+item[0]+")  "+item[1])))
+    form.register_college.choices = [item for item in college_arr]
+    if form.validate_on_submit() and request.method=='POST':
+        yearLvl = int(form.register_yearLvl.data)
+        db = models.students(id_number = form.register_id.data, 
+                            firstname = form.register_fname.data, 
+                            lastname = form.register_lname.data,
+                            yearlvl = yearLvl, 
+                            gender = form.register_gender.data , 
+                            course = form.register_course.data)
+        db.add()
+        return redirect(url_for('home'))
     return render_template('register.html', banner='Add Student',title='Register', form=form)
-
-    '''if request.method == "POST" and form.validate_on_submit():
-        student = models.students(id_number=form.register_id.data, firstname=form.register_fname.data, lastname=form.register_lname.data, course=form.register_course.data)
-        student.add()
-        return redirect(url_for('searched', id_number=form.register_id.data))
-    else:
-        return render_template('register.html', banner='Add Student',title='Register', form=form)'''
 
 
 
 
 @app.route('/update/<string:id_number>', methods=['GET', 'POST'])
 def update(id_number):
-    current_id.clear()
-    student = models.students(id_number=id_number)
-    students = student.search()
     form = updateForm()
+    current_id.clear()
+    courses = []
+    db = models.students(id_number=id_number)
+    college = db.showCollege()
+    college_arr = [('','Choose...'),]
+    for item in college:
+        college_arr.append((item[0],("("+item[0]+")  "+item[1])))
+    form.update_college.choices = [item for item in college_arr]
+    students = db.search()  
     banner_data = dict
     for item in students:
         banner_data = item
         current_id.append(item[0])
         if request.method=="POST" and form.validate_on_submit():
-            student = models.students(id_number=form.update_id.data, firstname=form.update_fname.data, lastname=form.update_lname.data, course=form.update_course.data, id=id_number)
-            student.update()
+            yearLvl = int(form.update_yearLvl.data)
+            db = models.students(id_number = form.update_id.data, 
+                            firstname = form.update_fname.data, 
+                            lastname = form.update_lname.data,
+                            yearlvl = yearLvl, 
+                            gender = form.update_gender.data , 
+                            course = form.update_course.data,
+                            id=id_number)
+            db.update()
             return redirect(url_for('searched', id_number=form.update_id.data))
+
         elif request.method == "GET":
-            form.update_id.data = item[0]
+            curr_course = item[3]
+            if item[6] != 'SGS':
+                courses.clear()
+                db_dept = models.students(college=item[6],dept=item[5])
+                dept = db_dept.showDept()
+                course = db_dept.showCourse()
+                form.update_department.choices = [(item[1]) for item in dept]
+                for i in course:
+                    courses.append((i[1],(i[1]+" "+i[2])))
+                form.update_course.choices = [(item) for item in courses]
+            else:
+                courses.clear()
+                db_dept = models.students(college=item[6],dept=item[5])
+                dept = db_dept.showSGSdept()
+                course = db_dept.showSGScourse()
+                form.update_department.choices = [(item[1]) for item in dept]
+                for i in course:
+                    courses.append((i[1],(i[1]+" "+i[2])))
+                form.update_course.choices = [(item) for item in courses]
+
             form.update_fname.data = item[1]
             form.update_lname.data = item[2]
-            form.update_course.data = item[3]
-
+            form.update_id.data = item[0]
+            form.update_gender.data =  item[9]
+            form.update_college.data = item[6]
+            form.update_yearLvl.data = str(item[4])
+            form.update_department.data = item[5]
+            form.update_course.data = curr_course
 
     banner = "Hi, " + banner_data[1]
     title = banner_data[1] + " " + banner_data[2]
     return render_template('update.html', banner=banner, title=title, form=form)
 
 
-@app.route('/dept/<string:get_dept>')
-def deptByCollege(get_dept):
-    if get_dept != 'SGS':
-        db = models.students(college=get_dept)
-        dept = db.showDept()
+@app.route('/dept/<string:get_college>')
+def deptByCollege(get_college):
+    form = registerForm()
 
-        deptArray = []
+    if get_college != 'SGS':
+        db = models.students(college=get_college)
+        dept = db.showDept()
+        form.register_department.choices = [(item[1]) for item in dept]
+
+        deptArray = [{
+          "college_code": "Choose...", 
+          "id": 0, 
+          "name": "Choose...", 
+          "name_value": ""
+        }]
         for item in dept:
             deptObj = {}
             deptObj['id'] = item[0]
+            deptObj['name_value'] = item[1]
             deptObj['name'] = item[1]
             deptObj['college_code'] = item[2]
             deptArray.append(deptObj)
         return jsonify({'department':deptArray})
     else:
-        db = models.students(college=get_dept)
+        db = models.students(college=get_college)
         dept = db.showSGSdept()
-
-        deptArray = []
+        form.register_department.choices = [(item[1]) for item in dept]
+        deptArray = [{
+          "college_code": "Choose...", 
+          "id": 0, 
+          "name": "Choose...", 
+          "name_value": " "
+        }]
         for item in dept:
             deptObj = {}
             deptObj['id'] = item[0]
+            deptObj['name_value'] = item[1]
             deptObj['name'] = item[1]
             deptObj['college_code'] = item[2]
             deptArray.append(deptObj)
         return jsonify({'department':deptArray})
 
 
-@app.route('/course/<string:get_course>')
-def courseByDept(get_course):
-    db = models.students(dept=get_course)
-    course = db.showCourse()
+@app.route('/course/<string:get_college>/<string:get_dept>')
+def courseByDept(get_college, get_dept):
+    form = registerForm()
+    if get_college != 'SGS':
+        db = models.students(dept=get_dept)
+        course = db.showCourse()
+        
+        courseArray = [{"code": "", 
+          "college_code": "", 
+          "department": "Choose...", 
+          "name": "Choose..."
+        }
+        ]
+        for item in course:
+            courseObj = {}
+            courseObj['department'] = item[0]
+            courseObj['code'] = item[1]
+            courseObj['name'] = item[2]
+            courseObj['college_code'] = item[4]
+            courseArray.append(courseObj)
+        return jsonify({'course':courseArray})
+    else:
+        db = models.students(dept=get_dept)
+        course = db.showSGScourse()
+       
+        courseArray = [{"code":"", 
+          "college_code": "", 
+          "department": "Choose...", 
+          "name": "Choose..."
+        }
+        ]
+        for item in course:
+            courseObj = {}
+            courseObj['department'] = item[0]
+            courseObj['code'] = item[1]
+            courseObj['name'] = item[2]
+            courseObj['college_code'] = item[4]
+            courseArray.append(courseObj)
+        return jsonify({'course':courseArray})
 
-    courseArray = []
-    for item in course:
-        courseObj = {}
-        courseObj['department'] = item[0]
-        courseObj['code'] = item[1]
-        courseObj['name'] = item[2]
-        courseObj['college_code'] = item[4]
-        courseArray.append(courseObj)
-    return jsonify({'course':courseArray})
    
-
-
-
-
-
-
-
-
-
-
-
-
+ 
+       
 
 
 @app.route('/searched/<string:id_number>', methods=['GET'])
