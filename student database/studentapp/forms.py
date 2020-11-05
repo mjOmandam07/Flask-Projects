84cfb35fe46ebd75cfa550df0310a60fbc267bf4
mjOmandam07 <mjomandam@gmail.com>
2020-11-05 22:44:38 +0800

    made a custom selectField to be used for register and update and added some value to the choices at all selectField. Added a custom validator for the selectField

--- student database/studentapp/forms.py
+++ student database/studentapp/forms.py
@@ -8,37 +8,55 @@ def validate_id(FlaskForm, field):
     student = models.students(id_number=field.data)
     if student.validation():
         raise ValidationError('I.D Number already exists')
+def validate_selectField(FlaskForm, field):
+    if field.data == 'Choose...' or field.data == '':
+        raise ValidationError('Please Select one of the options provided')
 
 
+
+
+class myCustomSelectField(SelectField):
+    def pre_validate(self, form):
+        pass
 
 
 class registerForm(FlaskForm):
-    register_id = StringField('I.D Number', validators=[DataRequired(), validate_id, Regexp(regex=r"^[0-9]{4}-[0-9]{4}$", message="Please input a valid I.D Number")])
     register_fname = StringField('First Name', validators=[DataRequired()])
     register_lname = StringField('Last Name', validators=[DataRequired()])
-    register_gender = SelectField('Gender', choices=[('Male'),('Female')], validate_choice = True)
-    register_yearLvl = SelectField('Year Level', choices=[1,2,3,4,5], validate_choice = True)
-    register_college = SelectField('College', choices=[], validate_choice = True)
-    register_department = SelectField('Department', choices=[], validate_choice = True)
-    register_course = SelectField('Course', choices=[], validate_choice = True)
+    register_id = StringField('I.D Number', validators=[DataRequired(), validate_id, Regexp(regex=r"^[0-9]{4}-[0-9]{4}$", message="Please input a valid I.D Number")])
+
+
+    register_gender = myCustomSelectField('Gender', choices=[('','Choose...'),('Male','Male'),('Female','Female')], validators=[DataRequired(), validate_selectField])
+    register_yearLvl = myCustomSelectField('Year Level', choices=[('','Choose...'),(1,1),(2,2),(3,3),(4,4),(5,5)], validators=[DataRequired(), validate_selectField])
+    register_college = myCustomSelectField('College', choices=[], validators=[DataRequired(), validate_selectField])
+    register_department = myCustomSelectField('Department', choices=[], validators=[DataRequired(), validate_selectField])
+    register_course = myCustomSelectField('Course', choices=[], validators=[DataRequired(), validate_selectField])
+
     register_submit = SubmitField('Add New Student')
 
 
+
+
 class updateForm(FlaskForm):
-    update_id = StringField('I.D Number', validators=[DataRequired(), validate_id, Regexp(regex=r"^[0-9]{4}-[0-9]{4}$", message="Please input a valid I.D Number")])
     update_fname = StringField('First Name', validators=[DataRequired()])
     update_lname = StringField('Last Name', validators=[DataRequired()])
-    update_gender = SelectField('Gender', choices=[('Male'),('Female')], validate_choice = True)
-    update_yearLvl = SelectField('Year Level', choices=[1,2,3,4,5])
-    update_college = SelectField('College', choices=[], validate_choice = True)
-    update_department = SelectField('Department', choices=[], validate_choice = True)
-    update_course = SelectField('Course', choices=[], validate_choice = True)
+    update_id = StringField('I.D Number', validators=[DataRequired(), validate_id, Regexp(regex=r"^[0-9]{4}-[0-9]{4}$", message="Please input a valid I.D Number")])
+
+
+    update_gender = myCustomSelectField('Gender', choices=[('','Choose...'),('Male','Male'),('Female','Female')], validators=[DataRequired(), validate_selectField])
+    update_yearLvl = myCustomSelectField('Year Level', choices=[('','Choose...'),(1,1),(2,2),(3,3),(4,4),(5,5)], validators=[DataRequired(), validate_selectField])
+    update_college = myCustomSelectField('College', choices=[], validators=[DataRequired(), validate_selectField])
+    update_department = myCustomSelectField('Department', choices=[], validators=[DataRequired(), validate_selectField])
+    update_course = myCustomSelectField('Course', choices=[], validators=[DataRequired(), validate_selectField])
+
     update_submit = SubmitField('Update')
 
 class filterForm(FlaskForm):
-    filter_college = SelectField('College', choices=[], validate_choice = True)
-    filter_department = SelectField('Department', choices=[], validate_choice = True)
-    filter_course = SelectField('Course', choices=[], validate_choice = True)
-    filter_arrange = SelectField('Arrange by', choices=['ID', 'Last Name', 'Course'], validate_choice = True)
+    filter_college = SelectField('College', choices=[], validators=[DataRequired()])
+    filter_department = SelectField('Department', choices=[], validators=[DataRequired()])
+    filter_course = SelectField('Course', choices=[], validators=[DataRequired()])
+    filter_arrange = SelectField('Arrange by', choices=['ID', 'Last Name', 'Course'], validators=[DataRequired()])
     filter_submit = SubmitField('Submit Filter')
+
+
 
